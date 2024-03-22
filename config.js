a445d479439ba12a1a04c7505102e096017e3ac6
Nav <62173713+codeaye@users.noreply.github.com>
2024-03-22 22:09:29 +0000

    e

---
+++ config.js
@@ -0,0 +1,191 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+  // ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+  // ├┴┐├─┤└─┐││  └─┐
+  // └─┘┴ ┴└─┘┴└─┘└─┘
+
+  // General
+  name: 'Nav',
+  imageBackground: false,
+  openInNewTab: false,
+  twelveHourFormat: false,
+
+  // Greetings
+  greetingMorning: 'Good morning!',
+  greetingAfternoon: 'Good afternoon,',
+  greetingEvening: 'Good evening,',
+  greetingNight: 'Go to sleep!',
+
+  // Layout
+  bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+
+  // Weather
+  weatherKey: 'ff4de683309586302651e9d8a2ad278b', // Write here your API Key
+  weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+  weatherUnit: 'C', // 'F', 'C'
+  language: 'en', // More languages in https://openweathermap.org/current#multi
+
+  trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+  defaultLatitude: '51.398739',
+  defaultLongitude: '0.457690',
+
+  // Autochange
+  autoChangeTheme: true,
+
+  // Autochange by OS
+  changeThemeByOS: true,
+  theme: 'latte-mocha',
+
+  // Autochange by hour options (24hrs format, string must be in: hh:mm)
+  changeThemeByHour: false,
+  hourDarkThemeActive: '18:30',
+  hourDarkThemeInactive: '07:00',
+
+  // ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+  // ├┴┐│ │ │  │ │ ││││└─┐
+  // └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+  firstButtonsContainer: [
+    {
+      id: '1',
+      name: 'Github',
+      icon: 'github',
+      link: 'https://github.com/',
+    },
+    {
+      id: '2',
+      name: 'Mail',
+      icon: 'mail',
+      link: 'https://inbox.google.com/',
+    },
+    {
+      id: '3',
+      name: 'Youtube',
+      icon: 'youtube',
+      link: 'https://youtube.com',
+    },
+    {
+      id: '4',
+      name: 'Calendar',
+      icon: 'calendar',
+      link: 'https://calendar.google.com/calendar/r',
+    },
+    {
+      id: '5',
+      name: 'Leet Code',
+      icon: 'code',
+      link: 'https://leetcode.com/problemset/all/',
+    },
+    {
+      id: '6',
+      name: 'Instagram',
+      icon: 'instagram',
+      link: 'https://www.instagram.com/?hl=en',
+    }
+  ],
+
+  secondButtonsContainer: [
+    {
+      id: '1',
+      name: 'Music',
+      icon: 'headphones',
+      link: 'https://open.spotify.com',
+    },
+    {
+      id: '2',
+      name: 'twitter',
+      icon: 'twitter',
+      link: 'https://twitter.com/',
+    },
+    {
+      id: '3',
+      name: 'bot',
+      icon: 'bot',
+      link: 'https://discord.com/app',
+    },
+  ],
+
+  // ┬  ┬┌─┐┌┬┐┌─┐
+  // │  │└─┐ │ └─┐
+  // ┴─┘┴└─┘ ┴ └─┘
+
+  // First Links Container
+  firstlistsContainer: [
+    {
+      icon: 'github',
+      id: '1',
+      links: [
+        {
+          name: 'Front',
+          link: 'https://www.reddit.com/r/Frontend/',
+        },
+        {
+          name: 'Rust',
+          link: 'https://www.reddit.com/r/rust/',
+        },
+        {
+          name: 'Go',
+          link: 'https://www.reddit.com/r/golang/',
+        },
+        {
+          name: 'Repos',
+          link: 'https://github.com/codeaye',
+        },
+      ],
+    },
+    {
+      icon: 'coffee',
+      id: '2',
+      links: [
+        {
+          name: 'Linkedin',
+          link: 'https://www.linkedin.com',
+        },
+        {
+          name: 'Dribbble',
+          link: 'https://www.dribbble.com',
+        },
+        {
+          name: 'Trello',
+          link: 'https://www.trello.com',
+        },
+        {
+          name: 'Slack',
+          link: 'https://www.slack.com',
+        },
+      ],
+    },
+  ],
+
+  // Second Links Container
+  secondListsContainer: [
+    {
+      icon: 'binary',
+      id: '1',
+      links: [
+        {
+          name: 'Spotify',
+          link: 'https://www.spotify.com',
+        },
+        {
+          name: 'Reddit',
+          link: 'https://www.reddit.com',
+        },
+        {
+          name: 'Hashnode',
+          link: 'https://www.hashnode.com',
+        },
+        {
+          name: 'Pocket',
+          link: 'https://www.pocket.com',
+        },
+      ],
+    },
+  ],
+};
